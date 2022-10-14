420f19cc22745aad72a355ce346be4a99bfbeafc
karengcecena <karengcecena@gmail.com>
2022-10-13 17:32:11 -0700

    Lab Completed

--- static/js/homepage.jsx
+++ static/js/homepage.jsx
@@ -1,7 +1,13 @@
 'use strict';
 
 function Homepage() {
-  return <div>This component needs to be implemented.</div>;
+  return (
+  <div>
+    <p>Welcome to our trading cards site!</p>
+    <a href="/cards">Click here to view our cards</a>
+    <img src="/static/img/balloonicorn.jpg"></img>
+  </div>
+  );
 }
 
 ReactDOM.render(<Homepage />, document.querySelector('#app'));
